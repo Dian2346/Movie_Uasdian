681ddd917a6d9ede323108d8b31ee6614282021a
Dian2346 <diannurvia029@gmail.com>
2023-07-11 19:53:58 +0700

    Initial commit

---
+++ app/src/main/java/com/dian/moviecatalog/models/MovieResponse.kt
@@ -0,0 +1,15 @@
+package com.dian.moviecatalog.models
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import com.dian.moviecatalog.models.Movie
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MovieResponse(
+    @SerializedName("results")
+    val movies : List<Movie>
+
+) : Parcelable {
+    constructor() : this(mutableListOf())
+}
